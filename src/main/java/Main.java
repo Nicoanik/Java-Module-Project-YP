cd69cda991cdc0107a41eb5f86b0d670a09e922d
Nicoanik <Nicoanik@hotmail.com>
2025-02-16 12:04:23 +0300

    Проектная работа №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashSet<Car> carList = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название автомобиля №" + i + ":");
+            String name = scanner.next();
+            System.out.println("Введите скорость автомобиля " + name + " (от 0 до 250):");
+            int speed = scanner.nextInt();
+            Car car = new Car(name, speed);
+            carList.add(car);
+            Race.newRaceLeader(name, speed);
+        }
     }
 }
